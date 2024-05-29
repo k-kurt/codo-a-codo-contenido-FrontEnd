3ace44695b5043c71f791d3bda0946eefecdce5c
Kevin <sboboda1997@gmail.com>
2024-05-29 02:15:02 -0300

    video 16 min 7:27

---
+++ JAVASCRIPT/CLASE16_js5/index.js
@@ -0,0 +1,23 @@
+document.write("hoola")
+
+
+
+//  ARRAYS
+
+let array=["a","b","c","d"];
+
+let mezcla=['hola',12,12.4,true]
+console.log(mezcla)
+
+console.log(mezcla[0])
+console.log(mezcla[2])
+
+//el ultimo elemento del array
+
+console.log(array[array.length-1])
+
+
+//CREACION MEDIANTE OBJETO ARRAY, DE LA FORMA TRADICIONAL
+
+const vectorDos=new Array("a","b","c")
+
