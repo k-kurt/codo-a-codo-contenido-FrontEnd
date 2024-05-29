29336d2af0ddb2939d6d869a55c73a20a5fe8429
k-kurt <sboboda1997@gmail.com>
2024-05-29 15:50:11 -0300

    video 16 min 20:36

--- JAVASCRIPT/CLASE16_js5/index.js
+++ JAVASCRIPT/CLASE16_js5/index.js
@@ -1,23 +1,53 @@
-document.write("hoola")
+// document.write("hoola")
 
 
 
 //  ARRAYS
 
-let array=["a","b","c","d"];
+// let array=["a","b","c","d"];
 
-let mezcla=['hola',12,12.4,true]
-console.log(mezcla)
+// let mezcla=['hola',12,12.4,true]
+// console.log(mezcla)
 
-console.log(mezcla[0])
-console.log(mezcla[2])
+// console.log(mezcla[0])
+// console.log(mezcla[2])
 
 //el ultimo elemento del array
 
-console.log(array[array.length-1])
+// console.log(array[array.length-1])
 
 
 //CREACION MEDIANTE OBJETO ARRAY, DE LA FORMA TRADICIONAL
 
-const vectorDos=new Array("a","b","c")
+// const vectorDos=new Array("a","b","c")
 
+
+
+//MUESTRA CON BUCLE
+
+// console.log("conponenetes del array: ")
+// for (let index = 0; index < array.length; index++) {
+//     console.log(array[index])
+    
+// }
+
+
+//METODOS CON ARRAYS
+
+var frutas=["banana","manzana"]
+
+console.log(frutas)
+
+//agregar elemento al final del array
+frutas.push("uvas", "naranja")
+console.log(frutas)
+
+frutas.push("hola","mandando fruta", [1,2,3,4])
+console.log(frutas[6])
+
+//acceder a los elementos de un array dentro de otro array, un array multidimensional
+console.log(frutas[6][1]);
+
+//eliminar el ultimo elemento de un arry
+frutas.pop();
+console.log(frutas)
