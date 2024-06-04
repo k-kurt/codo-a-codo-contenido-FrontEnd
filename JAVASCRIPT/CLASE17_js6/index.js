644f569de051249fdc4444dcbfd86ca029b940fc
Kevin <sboboda1997@gmail.com>
2024-06-04 03:06:58 -0300

    video 17 min 6

--- JAVASCRIPT/CLASE17_js6/index.js
+++ JAVASCRIPT/CLASE17_js6/index.js
@@ -3,3 +3,11 @@
 /* 
 VAMOS A VER DOM
 */
+
+
+//llamar a un elemento del hmtl a traves del name de la clase
+//siempre va el document al principio en el manejo del DOM
+let texto=document.getElementsByClassName('texto');
+
+//al llamarlo se crea un array de html, muestra que hay tres elementos con la clase 'texto'
+console.log(texto);
