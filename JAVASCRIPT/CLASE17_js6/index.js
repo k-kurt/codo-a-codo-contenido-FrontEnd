908084c521dfcaf303473443aa1dd2366631a16e
Kevin <sboboda1997@gmail.com>
2024-06-04 14:55:19 -0300

    video 17 min 10:34

--- JAVASCRIPT/CLASE17_js6/index.js
+++ JAVASCRIPT/CLASE17_js6/index.js
@@ -11,3 +11,19 @@ let texto=document.getElementsByClassName('texto');
 
 //al llamarlo se crea un array de html, muestra que hay tres elementos con la clase 'texto'
 console.log(texto);
+
+
+//puede contener varios tipos de elementos , en este caso el h1 junto con los p
+// console.log(document.getElementsByClassName('texto1'));
+
+
+//podemos traer un solo elemtno como en un array
+// console.dir(texto[0])//muestra las propiedades
+// console.log(texto[0].textContent)
+
+
+//modificando texto desde js
+texto[1].innerHTML="Modficando texto desde js";
+
+
+
