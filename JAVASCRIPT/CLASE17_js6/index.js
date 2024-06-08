8ec2a5263ca2cea9d8420af050261a02851aebed
k-kurt <sboboda1997@gmail.com>
2024-06-08 16:02:17 -0300

    query selector

--- JAVASCRIPT/CLASE17_js6/index.js
+++ JAVASCRIPT/CLASE17_js6/index.js
@@ -57,10 +57,30 @@ for(let x=0;x<texto.length;x++){
 //TRAER EL CONTENIDO DE LOS CAMPOS DEL FORMULARIO
 //a los imput generalmente le podemos poner un name, y lo podemos traer desde el javascript
 
-
+/* 
 let MostrarValor= ()=>{
     //hay que agregar el 0 por que la funcion usada trae un array no te trae un solo elemento, y el value es traer el valor de ese elemento 0 
     let nombre=document.getElementsByName("usurname")[0].value;
     let email=document.getElementsByName("email")[0].value;
     console.log(nombre, email)
-} 
+}  */
+
+
+
+
+    //QUERY SELECTOR
+
+    //con query selector podemos tomar todo el html, solo que hay que llamarlo como lo hariamos en css
+    //por ejemplo la clase con el punto, el id con #
+    //el promblema con query selector es que muestra solamente con lo primero que encutra, no crea un array o coleccion
+    //si queremos agarrar un name tenemos que poner document.queryselector('name="texto"')
+
+    let parrafo=document.querySelector('.text_parraf')
+    console.log(parrafo.textContent)
+
+    //tenemos el queryselectorAll que este si crea un nodo o array con todos los elementos con esa clase
+    let parrafo1=document.querySelectorAll('.text_parraf')
+    console.log("mostrando parrafo1",parrafo1)
+    for(let i=0;i<parrafo1.length;i++){
+        console.log(parrafo1[i].textContent)
+    }
