a0bfddd60da7f0943001b52f1dba6d729e252293
k-kurt <sboboda1997@gmail.com>
2024-06-13 01:36:56 -0300

    video 18

---
+++ JAVASCRIPT/CLASE18_js7/index.js
@@ -0,0 +1,8 @@
+/* CLASE DE SINCRONISMO 
+En js el sincronismo se refiere a la ejecucion secuencial de codigo, donde cada linea de codigo espera a que se la linea anterior se haya completado antes de ejecutarse
+El bloqueo:
+el modelo sincronico puede conducir a problemas de bloqueo de la interfaz de usuario, especialmente en aplicaciones web donde una tarea larga puede hacer que la pagina no responda
+La previsibilidad:
+el codigo sincronico es generalmente mas facil de leer, escribir y depurar debido a su naturaleza secuencial y predecible
+
+*/
