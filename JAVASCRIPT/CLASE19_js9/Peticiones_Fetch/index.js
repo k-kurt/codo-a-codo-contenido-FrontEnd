81395961a148e87d6671b0522df979143431a803
Kevin <sboboda1997@gmail.com>
2024-06-18 02:57:31 -0300

    empezando video 19

---
+++ JAVASCRIPT/CLASE19_js9/Peticiones_Fetch/index.js
@@ -0,0 +1,13 @@
+
+
+
+//PETICIONES FETCH
+
+/*
+El Fetch API es una tecnologia moderna que permite realizar solicitudes HTTP de manera asincronica desde el navegador. es parte del estandar de HTML5 y proporciona una manera poderosa y flexible de interactuar con seervidores web. A diferencia de XMLHttpRequest, que era la forma anterior de reallizar solicitudes, fetch ofrece una forma mas sencilla y potente de trabajar con promesas, facilitando la escritura de codigo asincrono que es mas facil de leer y mantener.
+
+Fetch API es una herramienta esencial para los desarrolladores web modernos, proporcionando una interfaz limpia y facil de usar para realiar solicituddes HTTP. A lo largo de esta clase, aprenderemos a utilizar fetch en diferentes escenarios, manejar respuestas de diversos tipos, y gestionar errores de manera efectiva. 
+ */
+
+
+
