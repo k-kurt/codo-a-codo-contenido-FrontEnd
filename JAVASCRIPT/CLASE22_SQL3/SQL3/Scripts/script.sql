354fa3ba91ab63ada9e9ab21efc7abe0da7d203d
Kevin <sboboda1997@gmail.com>
2024-06-26 16:35:46 -0300

    video 22 minuto 44

--- JAVASCRIPT/CLASE22_SQL3/SQL3/Scripts/script.sql
+++ JAVASCRIPT/CLASE22_SQL3/SQL3/Scripts/script.sql
@@ -3,6 +3,7 @@
 
 --RELACIONES ENTRE LAS TABLAS 
 
+--FOREING KEY
 --realizar una tabla pedido que traiga el id del cliente y el id del producto, que se hizo la compra
 --tenemos la tabla cliente y productos
 
@@ -16,4 +17,24 @@ CREATE TABLE pedido(
     foreign key (id_producto) references producto(codigo)
 
 )
+
+
+
+
+
+--INSERTAR DATOS EN TABLA CON FOREING KEY
+INSERT INTO pedido (fecha, id_producto, id_cliente)
+VALUES ("2024-05-26",1,2), ("2023-02-26",5,3), ("2023-01-2",6,5)
+
+
+
+
+
+--INNER JOIN
+--traer quines hicieron compras de la tabla clientes que estan en la tabla pedidos
+SELECT c.id, c.nombre
+FROM cliente c
+INNER JOIN pedido p ON c.id= p.id_cliente;
+
+
 
