aeeaa8feb54a46b64198643240007857342c3a4f
k-kurt <sboboda1997@gmail.com>
2024-06-28 00:59:52 -0300

    video 24 min 2

---
+++ JAVASCRIPT/CLASE24_NodeJS2/modulos/index.js
@@ -0,0 +1,14 @@
+
+
+//MODULO
+//nos sirve para llamarla desde otra hoja de js y poder usar esa funcion en otra clase
+
+
+function sumar(a,b){
+    return a+b;
+}
+
+
+module.exports=sumar
+
+//basicamente lo que estamos diciendo es que la funcion sumar se va  aexportar en un modulo
