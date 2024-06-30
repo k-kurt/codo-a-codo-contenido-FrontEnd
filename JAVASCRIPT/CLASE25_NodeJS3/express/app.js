717ad534cd602ed449bef5bcdaebd8f1e59ce105
k-kurt <sboboda1997@gmail.com>
2024-06-30 16:12:26 -0300

    minuto 15

---
+++ JAVASCRIPT/CLASE25_NodeJS3/express/app.js
@@ -0,0 +1,7 @@
+/* En la terminal corremos el comando:
+npm init -y
+Instalamos Express mediante npm:
+npm install express --save
+El flag --save indica que debe registrar la dependencia y sus subdependencias actualizadas.
+A continuación, vemos en el package.json que contamos con una lista de dependencias con express en la versión que acaba de instalar
+ */
