640d688ae7ac662fd6b40ef3d79c6df423a3a955
k-kurt <sboboda1997@gmail.com>
2024-06-30 00:05:04 -0300

    min 29

--- JAVASCRIPT/CLASE24_NodeJS2/modulos/servidor_estatico/index.js
+++ JAVASCRIPT/CLASE24_NodeJS2/modulos/servidor_estatico/index.js
@@ -2,5 +2,21 @@
 const http=require('http')
 
 //necesitamos crear una variable donde levantamos ese servidor
-const server=http.createServer()
+const server=http.createServer(
+    (req, res)=>{//el requiare se trata de un pedido al servidor,en este caso no esperamos un req pero si una respuesta 
+        // res.writeHead(200,{'content-Type': 'text/plain'})//el contenido que va a recibir sera texto plano
+
+        // res.end('hola mundo')//la respuesta final sera el hola mundo
+        //basicamente mostrara una pantalla engra con el mensaje hola mundo
+
+        //intentamos con un elemento html
+        res.writeHead(200,{'content-Type': 'text/html; charset=UTF-8'})
+        res.end('<h1>hola mundo!</h1><p>parrafo</>')
+    }
+)
+
+
+//para levantar el servidor necesitamos el escuchador
+server.listen(3000, ()=> console.log('Servidor corriendo en puerto http://localhost:3000'));//pasamos el puerto donde corre y una arraow function
+
 
---
+++ JAVASCRIPT/CLASE24_NodeJS2/modulos/servidor_estatico/index2.js
@@ -0,0 +1,16 @@
+//importamos nuestro modulo http
+const http=require('http')
+
+
+const server=http.createServer(
+    (req, res)=>{
+        res.writeHead(200,{'content-Type': 'text/html; charset=UTF-8'})
+        res.end('<h1>hola mundo!</h1><p>parrafo</>')
+    }
+)
+
+
+//para levantar el servidor necesitamos el escuchador
+server.listen(3000, ()=> console.log('Servidor corriendo en puerto http://localhost:3000'));
+
+
