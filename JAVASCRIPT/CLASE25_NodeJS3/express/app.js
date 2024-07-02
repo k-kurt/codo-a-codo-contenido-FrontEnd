0381ddacaf4715d3bc0986147f7ede76d7fabc7d
k-kurt <sboboda1997@gmail.com>
2024-07-02 14:25:38 -0600

    minuto 19 video 25

--- JAVASCRIPT/CLASE25_NodeJS3/express/app.js
+++ JAVASCRIPT/CLASE25_NodeJS3/express/app.js
@@ -1,7 +1,15 @@
-/* En la terminal corremos el comando:
+/*
+Tenemos que ubicarnos en la carpeta donde est el js y ahi instalar tood, luego:
+En la terminal corremos el comando:
+para crear la carpeta json, para incializar el proyecto
 npm init -y
 Instalamos Express mediante npm:
 npm install express --save
 El flag --save indica que debe registrar la dependencia y sus subdependencias actualizadas.
 A continuación, vemos en el package.json que contamos con una lista de dependencias con express en la versión que acaba de instalar
  */
+
+//llamamos a express
+const express=require('express')
+//vamos a crear una variable donde esten englobadas todas las funciones de express, llama a los metodos de express
+const app=express()
