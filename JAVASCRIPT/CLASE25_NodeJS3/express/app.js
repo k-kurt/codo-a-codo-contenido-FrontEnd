adb032f887a608155bd27c6665ce5eb6f1a588be
k-kurt <sboboda1997@gmail.com>
2024-07-12 17:12:07 +0200

    min 57

--- JAVASCRIPT/CLASE25_NodeJS3/express/app.js
+++ JAVASCRIPT/CLASE25_NodeJS3/express/app.js
@@ -16,11 +16,24 @@ const app=express()
 //tenemos que declarar el puerto donde vamos a correr la aplicacion
 const port=3000
 
+
+//Esto es para utilizar la carpeta public donde siempre se utiliza el index.html coo pagina inicial 
+app.use(express.static('public'))//hay que crear esa carpeta donde esta el app.js
+
+
 //vamos a empezar a usar los estados
 //el primer parametro que le vamos a pasar sera la ruta donde queremos levantar este servidor, podemos darle varias rutas y cada ruta puede tener una accion distinta, en este caso la ruta incio pero puede ser la que nosotros querramos
 app.get("/inicio", (req, res)=>{
     res.send("Hello word");//la respuesta a esa ruta va ser hello word
 })
+
+//esto lo usamos para acceder al segundo archivo html en la carpeta public, no es la mejor forma para acceder a html de la pagina, haremos otra forma, pero usamos el get para el pedido de datos
+app.get('/segundaRuta',(req, res)=>{
+    res.sendFile(__dirname+'/public/segundaRuta.html')
+})
+
+
+
 
 
 //llamamos al servidor
@@ -29,16 +42,21 @@ app.listen(port, ()=>{
 })
 
 /* 
-EN EL PACKAGE.JSON AGREGAMOS EL "start": "node.js" en la parte de scripts 
-Si queremos que la pagina se vaya actualizando a medida que se vaya codeando se usa Nodemon, como el liveserver funciona, no todas las pc lo soportan, una forma es parecida a lo que hicimos antes pero agregando el flag "--watch"
+-EN EL PACKAGE.JSON AGREGAMOS EL "start": "node.js" en la parte de scripts, esto es para lanazar el comando npm start 
+
+-Si queremos que la pagina se vaya actualizando a medida que se vaya codeando se usa Nodemon, como el liveserver funciona, no todas las pc lo soportan, una forma es parecida a lo que hicimos antes pero agregando el flag "--watch"
 "scripts" : {
 "start": "node --watch app.js"
 }
 esa es una forma de que lo ejecute javascript
-La otra forma es instalar la dependencia y hacer lo mismo
+
+-La otra forma es instalar la dependencia y hacer lo mismo
 se instala con "npm install -D nodemon"
 y se hace lo mismo que antes 
 "scripts": {
 "start": "nodemon app.js"
 }
+ 
+-Esto permite estar actualizando la pagina para ver los cambios, el nodemon va cerrando y abriendo el servidor para que nosotros no tengamos que hacerlo
+
 */
