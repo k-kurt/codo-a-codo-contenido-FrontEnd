fccf84a89f9183b3e42ecad102716915210512bd
k-kurt <sboboda1997@gmail.com>
2024-07-13 20:58:28 +0200

    video 26 min 28

---
+++ JAVASCRIPT/CLASE26_NodeJS4/server.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const app=express()
+const PORT=3000
+const moviesRouter=require('/routes/movies.js')
+
+
+app.use(express.json())//va a recibir un json
+app.use('/movies',moviesRouter)//es la ruta, donde nos va a esperar un array de objetos que vamos a ir pasando a esta direccion
+
+
+
+//--------------
+app.listen(PORT, ()=>{
+    console.log(`Corriendo servidor en el puerto ${PORT}`)
+})
