6d0c1112392a2d4d5b4362b8cfe1ea12e001d080
k-kurt <sboboda1997@gmail.com>
2024-07-16 14:23:08 +0200

    mysql2 instalado

--- JAVASCRIPT/CLASE27_NodeJS5/src/server.js
+++ JAVASCRIPT/CLASE27_NodeJS5/src/server.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const app=express()
 const PORT=3000
-const moviesRouter=require('./routes/movies.js')
+const moviesRouter=require('../routes/movies.js')
 
 
 app.use(express.json())//va a recibir un json
