2861a62522891925c76bce28590e2a5e3e3ee75c
k-kurt <sboboda1997@gmail.com>
2024-08-03 17:45:39 +0200

    video 27 min 20

--- JAVASCRIPT/CLASE27_NodeJS5/controllers/movieController.js
+++ JAVASCRIPT/CLASE27_NodeJS5/controllers/movieController.js
@@ -1,1 +1,11 @@
 //Este va a ser el controller donde vamos a manejar todo
+
+
+const db=require('../db/db.js')//sale, entra a la carpeta db y usa el archivo db donde va a estar la conexion a la bbdd
+
+
+const getAllMovies=(req, res) =>{
+    const query= 'SELECT  * FROM movies'
+    
+
+}
--- JAVASCRIPT/CLASE27_NodeJS5/routes/movies.js
+++ JAVASCRIPT/CLASE27_NodeJS5/routes/movies.js
@@ -4,8 +4,15 @@
 /* vamos a llamar al controlador, que es donde vamos a hacer las consultas y aqui vamos a llamar a esas consultas a traves de las funciones */
 const express=require('express')
 const router=express.Router()
+const movieController=require('../controllers/movieController')//aqui llamos al archivo que tendra las funciones para traer y modificar las pelis
 
 
+//vamos a usar la funcion del controller para traer todas las pelis
+router.get('/', movieController.getAllMovies)//tine que ser el mismo nombre de la funcion 
+// router.get('/:id', movieController.getMovieById)
+// router.post('/', movieController.createMovie)
+// router.put('/:id', movieController.updateMovie)
+// router.delete('/:id', movieController.deleteMovie)
 
 
 module.exports=router
