b4b2783393459c7aa63334db21dddcfb174db4b0
k-kurt <sboboda1997@gmail.com>
2024-08-09 13:01:20 +0200

    video 27 min 39 creacion de bbdd

--- JAVASCRIPT/CLASE27_NodeJS5/db/db.js
+++ JAVASCRIPT/CLASE27_NodeJS5/db/db.js
@@ -1,1 +1,23 @@
 //aqui va a estar alojada nuestra conexion a la base de datos
+
+
+const mysql=require('mysql2')
+
+const connection=mysql.createConnection({
+    //cuando levantamos la bbdd con xampp la contrasenia es vacia
+    host: 'localhost',
+    user: 'user',
+    password: '',
+    database: 'movies_db'
+})
+
+connection.connect((err)=>{
+    if(err){
+        console.log('Error en la base de datos', err)
+    return
+    }
+    console.log('Conectado a la base de datos')
+})
+
+
+module.exports=connection
