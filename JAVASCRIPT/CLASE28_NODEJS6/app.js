8c19691bbd21448fc1ac6bb63e585663bc5c3316
k-kurt <sboboda1997@gmail.com>
2024-08-31 22:17:36 -0300

    min 12:30

--- JAVASCRIPT/CLASE28_NODEJS6/app.js
+++ JAVASCRIPT/CLASE28_NODEJS6/app.js
@@ -1,7 +1,25 @@
 const express=require('expres')
 const app=express()
 const PORT=3000
+const multer=require('multer')
+
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null, 'uploads/')//el nombre tiene que ser igual a la carpeta
+    },
+    filename: (req,file,cb)=>{
+        cb(null,Date.now()+path.extname(file.originalname))
+    }
+    //el null es por si falla que no suba ningun archivo
+    
+})
+
+
+
 
 app.listen(PORT, ()=>{
     console.log('Corriendo en puerto 3000')
 })
+
+
+// el multer nos permite guardar archivos, en este caso lo haremos desde el postman, en vez de mandar un json, pasamos un jpg y este lo guardara en una carpeta que crearemos en el proyecto o tambien lo podemos almacenar en la bbdd que tambien lo almacena en el local, en la bbdd va el directorio del archivo
