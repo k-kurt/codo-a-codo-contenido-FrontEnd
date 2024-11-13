6c7ad29cf45bf4133b5ee72ae96823d04a692c54
k-kurt <sboboda1997@gmail.com>
2024-11-12 22:11:51 -0300

    clase 29 terminada

--- JAVASCRIPT/CLASE29_NODEJS7/routes/authRoutes.js
+++ JAVASCRIPT/CLASE29_NODEJS7/routes/authRoutes.js
@@ -1,1 +1,15 @@
- 
+ const express = require('express')
+ const authController=require('../controllers/authController')
+ const authMiddleware= require('../middlewars/authMiddleware')
+
+
+ const router=express.Router()
+
+ router.post('/register', authController.register)//primero tendremos que registrarnos 
+ router.post('/login', authController.login)//luego tendremos que loguearnos
+
+ router.get('/protected', authMiddleware, (req, res) => {
+    res.status(200).send(`Que onda ${req.userId}`)//si esta todo bien, nos abre la ruta protegida
+ })
+
+ module.exports=router
